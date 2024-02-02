638da2b4341e6e05f8d7cd472e01bd935a4e4ec1
Josh Faber <faberj@student.ncmich.edu>
2024-02-01 20:19:30 -0500

    Added readme

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using gitPractice.models;
+
+Person person = new Person();
+
+person.FirstName = "Josh";
+person.LastName = "Faber";
